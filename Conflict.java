28489e342ea3f0f4ff6799cbfeb3309e9c5567e2
masaya obara <m.obara1811@morijyobi.ac.jp>
2019-06-04 11:56:24 +0900

    [modify]Conflict.javaの修正

--- Conflict.java
+++ Conflict.java
@@ -1,6 +1,11 @@
+import jva.util.Ramdom;
 public class Conflict{
 	public static void main(String[] args){
-		System.out.println("HelloWorld");
+		Random rnd=new Random();
+		for(int i=0;i<10;i++){
+			int i=rnd.nextInt(100)+1;
+			System.out.println(i);
+		}
 	}
 }
 
